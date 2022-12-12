ddfc8938c765bd8cb487f9d4d14565c862982897
swert555555 <113428867+swert555555@users.noreply.github.com>
2022-12-12 12:28:46 +0300

    Add files via upload

--- Потоки/Class1.cs
+++ Потоки/Class1.cs
@@ -4,26 +4,48 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace Потоки
 {
     public class Top_Polsovatelei
     {
-        public string? Name;
-        public List<Polsovatel>? Vse_polsovateli;
+        public static string? Name = "Топ пользователей";
+        public static List<Polsovatel>? Vse_polsovateli;
+
+        
+        public static void ConvertJson(List<Polsovatel> pols)
+        {
+            Vse_polsovateli = pols;
+            string json = JsonConvert.SerializeObject(Vse_polsovateli);
+            File.WriteAllText("C:\\Users\\Kotam\\OneDrive\\Рабочий стол\\учеба\\C#\\Потоки\\he.json", json);
+        }
+        public static void ReadJSON()
+        {
+            string text = File.ReadAllText("C:\\Users\\Kotam\\OneDrive\\Рабочий стол\\учеба\\C#\\Потоки\\he.json");
+            Vse_polsovateli = JsonConvert.DeserializeObject<List<Polsovatel>>(text) ?? new List<Polsovatel>();
+            Console.WriteLine(Top_Polsovatelei.Name);
+            foreach (Polsovatel user in Vse_polsovateli)
+            {
+                Console.WriteLine("\nИмя: " + user.Name + "\nСимволов в секунду: " + user.Simvol_Sekund + "\nВсего символов: " + user.Vsego_Simvol);
+                Console.WriteLine("---------------------");
+            }
+        }
     }
 
     public class Polsovatel
     {
         public string? Name;
-        public static int Vsego_Simvol = 0;
-        private int Simvol_Minyta = Vsego_Simvol / 60;
+        public int Vsego_Simvol = 0;
+        public double Simvol_Sekund;
 
-        public Polsovatel(string Name)
+        public Polsovatel(string Name, double Simvol_Sekund, int Vsego_Simvol)
         {
             this.Name = Name;
+            this.Vsego_Simvol = Vsego_Simvol;
+            this.Simvol_Sekund = Simvol_Sekund;
         }
     }
-
-
 }
--- Потоки/Program.cs
+++ Потоки/Program.cs
@@ -7,55 +7,74 @@ namespace Потоки
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введи имя. После начнется обратный отсчет, твоя задача - печатать текст.");
-            Polsovatel chel = new(Name: Console.ReadLine());
-            List<Polsovatel> polsovatels = new List<Polsovatel>();
-            polsovatels.Add(chel);
-            Console.Clear();
-            string TextRead = "Почему от алкоголя люди кажутся красивее и сексуальнее? Исследование, опубликованное в 2003 году, показало, что мужчины и женщины, употреблявшие умеренное количество алкоголя, находили лица представителей противоположного пола на 25 процентов более привлекательными, чем их трезвые сверстники. На основе этого исследования ученые из Лондонского университета Рохэмпотна выяснили, что алкоголь ухудшает способность воспринимать асимметрию на лицах и снижает предпочтение симметричных лиц перед асимметричными. Ведь красота это что? Симметрия, то есть «правильные» черты лица и фигуры. На способность их распознавать и влияет алкоголь, меняя работу префронтальной коры. А его влияние на лобную долю усиливает наше желание сближаться с другими людьми. Последний гвоздь в крышку гроба: воздействие на вентральный стриатум, который включает импульсивное и нелогичное поведение. Почему это плохо? Ну не то чтобы плохо, скорее неэффективно. Я за то, чтобы любая коммуникация - особенно сексуальная - доставляла всем участникам максимум кайфа. А если вы на утро жалеете о содеянном, или, что ещё хуже, с содроганием смотрите на человека в постели - это уже не максимум. Про секс нужно вспоминать с наслаждением, а не с отвращением. Это если вы вообще его запомните, ведь на гиппокамп, хранилище памяти, алкоголь тоже плохо влияет. Чтобы не случалось всякой фигни, можете соблюдать золотое правило, первый секс с новым партнёром - на трезвую (или хотя бы полутрезвую) голову.";
-            string YourText = "к";
-            int Time = 60;
-            Polsovatel.Vsego_Simvol = YourText.Length;
+        public static List<Polsovatel> polsovatels = new();
 
-        Thread thread = new Thread(x =>
+        public static void Main()
+        {   
+            Console.WriteLine("Нажми F2");
+            for (ConsoleKeyInfo key = Console.ReadKey(); key.Key == ConsoleKey.F2; key = Console.ReadKey())
             {
+                Console.WriteLine("Введи имя:");
 
-                Console.SetCursorPosition(0, 1);
+                string? name = Console.ReadLine();
+                int Time = 60;
+                Console.Clear();
+                string TextRead = "Почему от алкоголя люди кажутся красивее и сексуальнее? Исследование, опубликованное в 2003 году, показало, что мужчины и женщины, употреблявшие умеренное количество алкоголя, находили лица представителей противоположного пола на 25 процентов более привлекательными, чем их трезвые сверстники. На основе этого исследования ученые из Лондонского университета Рохэмпотна выяснили, что алкоголь ухудшает способность воспринимать асимметрию на лицах и снижает предпочтение симметричных лиц перед асимметричными. Ведь красота это что? Симметрия, то есть «правильные» черты лица и фигуры. На способность их распознавать и влияет алкоголь, меняя работу префронтальной коры. А его влияние на лобную долю усиливает наше желание сближаться с другими людьми. Последний гвоздь в крышку гроба: воздействие на вентральный стриатум, который включает импульсивное и нелогичное поведение. Почему это плохо? Ну не то чтобы плохо, скорее неэффективно. Я за то, чтобы любая коммуникация - особенно сексуальная - доставляла всем участникам максимум кайфа. А если вы на утро жалеете о содеянном, или, что ещё хуже, с содроганием смотрите на человека в постели - это уже не максимум. Про секс нужно вспоминать с наслаждением, а не с отвращением. Это если вы вообще его запомните, ведь на гиппокамп, хранилище памяти, алкоголь тоже плохо влияет. Чтобы не случалось всякой фигни, можете соблюдать золотое правило, первый секс с новым партнёром - на трезвую (или хотя бы полутрезвую) голову. Света лох";
+                string YourText = "";
+
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine(TextRead); //DarkGray
-                Console.ForegroundColor = ConsoleColor.White;
-                int i = 0;
-                Console.SetCursorPosition(1, 0);
-                for (char console_char_text = Convert.ToChar(Console.Read()); Time != 0; console_char_text = Convert.ToChar(Console.Read()), i++)
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine(TextRead);
+                Console.WriteLine("\nНажми любую клавишу, чтобы начать");
+                Console.ReadKey();
+
+                Thread thread = new(x =>
                 {
-                    if (console_char_text == TextRead[i])
-                        YourText += console_char_text;
-                    //YourText = (string)YourText.Append(console_char_text);
-                    else
-                        i--;
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.SetCursorPosition(1, 0);
-                    Console.WriteLine(console_char_text);
-                    Console.WriteLine(YourText);
-                }
-            });
-            
-            thread.Start();
-            Stopwatch sw = Stopwatch.StartNew();
-                while (Time != 0)
+                    int i = 0;
+                    Console.SetCursorPosition(0, 1);
+                    for (char console_char_text = Console.ReadKey().KeyChar; Time != 50; i++, console_char_text = Console.ReadKey().KeyChar)
+                    {
+                        if (console_char_text == TextRead[i])
+                            YourText += TextRead[i];
+                        else
+                            i--;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.SetCursorPosition(0, 1);
+                        Console.Write(YourText);
+                    }
+                });
+
+                thread.Start();
+                Stopwatch sw = Stopwatch.StartNew();
+                while (Time != 50)
                 {
                     Time = Convert.ToInt32(60 - sw.ElapsedMilliseconds / 1000);
                     Console.SetCursorPosition(0, 0);
                     Console.WriteLine(Time);
-                    Thread.Sleep(1000);
+                    if (Time != 0)
+                        Thread.Sleep(1000);
                 }
-            Console.Clear();
-            Console.WriteLine("Время вышло! ...");
-
-            Console.WriteLine(YourText); //не меняется строка, не заполняется
-
+                Thread.Sleep(2000);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Время вышло! ...");
+                Polsovatel chel = new(name, Math.Round((double)YourText.Length / 60.0, 2), YourText.Length);
+                polsovatels.Add(chel);
+                Top_Polsovatelei.ConvertJson(polsovatels);
+                Console.WriteLine("Имя: " + chel.Name + "\nСимволов в секунду: " + chel.Simvol_Sekund + "\nВсего символов: " + chel.Vsego_Simvol);
+                Console.WriteLine("Для перехода в топ нажми f1. Для повторного прохождения - f2");
+            }
+            Klavisha();
+        }
+        private static void Klavisha()
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.F1)
+            {
+                Console.Clear();
+                Top_Polsovatelei.ReadJSON();
+            }
         }
     } 
 }
